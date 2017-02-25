9efe3c0284ff7eca0e09f8692ecf717cbff10b13
oyuk <okysoft68@gmail.com>
2017-02-25 13:27:59 +0900

    [from now] 2017/02/25 13:27:59
     docker-compose.yml                           |  2 +-
     {src/frontend => frontend}/package.json      |  4 ++--
     {src/frontend => frontend/public}/index.html |  0
     {src/frontend => frontend/src}/index.js      |  2 +-
     {src/frontend => frontend}/webpack.config.js | 16 +++++++++++++---
     5 files changed, 17 insertions(+), 7 deletions(-)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 ReactDOM.render(
-    <div>Hello workd</div>
+    <div>Hello world</div>,
     document.getElementById('app')
 );
--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    entry: './index.js',
+    entry: './src/index.js',
     output: {
-        path: __dirname,
+        path: './public',
         filename: 'bundle.js'
     },
     module: {
@@ -15,5 +15,15 @@ module.exports = {
                 }
             }
         ]
-    }
+    },
+    // devServer: {
+    //   contentBase: './public',
+    //   inline: true,
+    //   port: 8080,
+    //   historyApiFallback: true,
+    //   stats: {
+    //     version: false,
+    //     hash: false,
+    //     chunkModules: false,
+    // },
 }
